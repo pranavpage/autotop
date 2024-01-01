b0f0fc949dd472916bd5e3a05ebee1f47e55cc11
pranav page <pranavpage33@gmail.com>
2024-01-01 11:11:49 +0530

    Structure for prepare_data

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Automated Map Generation
 
 ## ROUGH
+[Q] should the functions for preparing the data be written from scratch?
+[A] yeah do it, nothing else is really intensive
 
 ## Actual Documentation starts here
 
---
+++ src/prepare_data.py
@@ -0,0 +1,17 @@
+# Script to generate data for training 
+# Segmentation images and label masks for buildings and roads
+
+
+# look at functions from create_spacenet_masks.py and apls_utils, write your own
+
+def extract_building_train_data():
+    return
+
+def extract_road_train_data():
+    return 
+
+def main():
+    return 
+
+if(__name__ == "__main__"):
+    main()
